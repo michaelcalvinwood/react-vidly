56f9a47096bb8318eb41f8c458d975cae5cb5b29
Michael Wood <michaelwood33311@icloud.com>
2023-01-28 16:00:04 -0500

    pagination.jsx added prop type checking

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 
 const Pagination = ({itemsCount, pageSize, currentPage, onPageChange}) => {
     console.log('Pagination currentPage', currentPage);
@@ -22,6 +23,13 @@ const Pagination = ({itemsCount, pageSize, currentPage, onPageChange}) => {
             </ul>
         </nav>
      );
+}
+
+Pagination.propTypes = {
+    itemsCount: PropTypes.number.isRequired, 
+    pageSize: PropTypes.number.isRequired, 
+    currentPage: PropTypes.number.isRequired, 
+    onPageChange: PropTypes.func.isRequired
 }
  
 export default Pagination;
